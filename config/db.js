96cc413181dfcedccf3fd7d85080e65562a9feab
Fuller11030 <fullerdawn2311030@gmail.com>
2025-05-17 18:32:16 -0600

    novena actualizacion

--- config/db.js
+++ config/db.js
@@ -1,6 +1,12 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PASSWORD', process.env.DB_PASSWORD);
+console.log('DB_PORT', process.env.DB_PORT);
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10) || 5432,
@@ -13,3 +19,7 @@ const pool = new Pool({
 });
 
 module.exports = pool;
+
+pool.connect()
+.then(() => console.log('Conectado a la base de datos'))
+.catch(err => console.error('Error al conectar a la base de datos:', err.message));
